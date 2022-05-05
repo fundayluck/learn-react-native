729f638cde2350cb8cbdae407602e8e99e7dca5a
jember08 <afandayul@gmail.com>
2022-05-05 12:24:46 +0700

    updating state

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
+  const [input, setInput] = useState("");
+  console.log(input);
   return (
     <View>
+      <Text>Enter Name:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
+        value={input}
+        onChangeText={(text) => setInput(text)}
       />
+      <Text>My Name is {input}</Text>
     </View>
   );
 };
